027df590219a4da9876fe588ab094acb428c406c
Michał Pawłowski <kontakt@pawlowskimichal.pl>
2025-08-18 15:08:06 +0200

    .

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    unoptimized: true,
+    domains: ["i.ytimg.com"],
+  },
+};
 
-module.exports = nextConfig;
+export default nextConfig;
